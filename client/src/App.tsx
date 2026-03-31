217f024df5d05c972e7a710ccd9b5ba701242132
hillaryelsaipoumb-creator <hillaryelsa.ipoumb@gmail.com>
2026-03-31 14:17:18 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,8 @@
 import { Switch, Route } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { Toaster } from "@/components/ui/toaster";
-import HomePage from "@/pages/home";
-import NotFound from "@/pages/not-found";
+import HomePage from "./pages/home";
+import NotFound from "./pages/not-found";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Route path="/" component={HomePage} />
         <Route component={NotFound} />
       </Switch>
-      <Toaster />
     </QueryClientProvider>
   );
 }
